4a6144d87f8f085902ba047f2b81fd27f5889b2e
sivakumarevuri <83264340+sivakumarevuri@users.noreply.github.com>
2023-07-28 10:27:52 +0530

    Update testplot.py

--- testplot.py
+++ testplot.py
@@ -1,5 +1,17 @@
+import os.path
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def test_plot(path):
+    location = os.path.join(path, 'student_data.csv')
+    data = pd.read_csv(location)
+    data.plot(x='Student_id', y='Percentage')
+
+    plt.savefig(os.path.join(path, 'graph.png'))
+    plt.show()
+
+
+#test_plot('/home/lucifer/Desktop/siva/data/')
 
 
